70fb6e266cf3546c00af6363d52f58f889db752e
Eudy Cabral Burgos <eudy_cabral@ucne.edu.do>
2018-06-17 13:12:48 -0400

    Ventana Diseño Registro Grupos Lista

---
+++ Registro con Detalles/UI/REGISTROS/Registro Grupos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Registro_con_Detalles.UI.REGISTROS
+{
+    public partial class Registro_Grupos : Form
+    {
+        public Registro_Grupos()
+        {
+            InitializeComponent();
+        }
+    }
+}
